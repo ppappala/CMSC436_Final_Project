39334384bd487735554b6e21fc75c441da0e4217
Pranav Pappala <pranavpappala@gmail.com>
2022-05-06 18:52:46 -0400

    Adding project

---
+++ app/src/main/java/com/example/parkmobilesurvey/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.parkmobilesurvey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.parkmobilesurvey.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/parkmobilesurvey/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.parkmobilesurvey.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
