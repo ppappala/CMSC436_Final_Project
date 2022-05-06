b78dd51492a888ef78a41cccdd73a6612024a0b9
Pranav Pappala <pranavpappala@gmail.com>
2022-05-06 19:01:29 -0400

    Adding project

--- app/src/main/java/com/example/parkmobilesurvey/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/parkmobilesurvey/ui/main/MainViewModel.kt
@@ -4,4 +4,6 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+
+    // Test comment added by Pranav to see if I can push changes to Github
 }
