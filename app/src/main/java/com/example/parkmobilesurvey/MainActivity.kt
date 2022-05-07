94da2dc3ab293d4df2b93df2849d5143f5ad414a
keyaroz <keyarozar@gmail.com>
2022-05-07 01:21:34 -0400

    keya connecting activities

--- app/src/main/java/com/example/parkmobilesurvey/MainActivity.kt
+++ app/src/main/java/com/example/parkmobilesurvey/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.parkmobilesurvey
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 import com.example.parkmobilesurvey.ui.main.MainFragment
+import com.example.surverypart2.SurveyPage2
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var next1: Button // will change after stephane put's files in
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // This will be a filler for the first survey page
+        // until Stephane put's his files in
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
@@ -14,6 +22,15 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+        next1 = findViewById(R.id.next1)
+
+        // To go to second survey page (third page of app)
+        next1.setOnClickListener {
+            val intent = Intent(this, SurveyPage2::class.java)
+            startActivity(intent);
+        }
+
+
 
 //        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
         // Pranav was here
--- app/src/main/java/com/example/parkmobilesurvey/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/parkmobilesurvey/ui/main/MainViewModel.kt
@@ -8,4 +8,6 @@ class MainViewModel : ViewModel() {
     // Test comment added by Pranav to see if I can push changes to Github
     // Another test comment added by Pranav
     // it
+    //keyaaa 10:35
+
 }
