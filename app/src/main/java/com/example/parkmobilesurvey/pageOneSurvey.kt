0d993e01adf60e6216208022d91af5e99d1d64c3
keyaroz <keyarozar@gmail.com>
2022-05-07 21:57:35 -0400

    keya commit 9:57

--- app/src/main/java/com/example/parkmobilesurvey/pageOneSurvey.kt
+++ app/src/main/java/com/example/parkmobilesurvey/pageOneSurvey.kt
@@ -1,5 +1,8 @@
 package com.example.parkmobilesurvey
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -13,10 +16,15 @@ class pageOneSurvey : AppCompatActivity() {
 
     private lateinit var complete: TextView
 
+    //added by keya
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page_one_survey)
+    // shared pref added by keya
+        sharedPreferences = getSharedPreferences("sharedPref", Context.MODE_PRIVATE);
+
         group1 = findViewById(R.id.radioGroup)
         group2 = findViewById(R.id.radioGroup2)
 
@@ -29,7 +37,20 @@ class pageOneSurvey : AppCompatActivity() {
             val check1 = group1.checkedRadioButtonId
             val check2 = group2.checkedRadioButtonId
 
+    // added by keya
+            //the checked radio button for first quesiton
+            val checkedRadio: Int = group1.checkedRadioButtonId
 
+            //the checked radio button for second quesiton
+            val checkedRadio2: Int = group2.checkedRadioButtonId
+
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+            editor.putInt("firstAns", checkedRadio)
+            editor.putInt("secondAns", checkedRadio2)
+            editor.apply();
+
+            //-----------------
             if(check1 < 0 || check2< 0){
                 Toast.makeText(this, "you must click all question", Toast.LENGTH_SHORT).show()
 
@@ -37,6 +58,12 @@ class pageOneSurvey : AppCompatActivity() {
                 complete.text = check1.toString() + check2.toString()
 
             }
+
+            val intent = Intent(this, pageTwoSurvey::class.java)
+
+            startActivity(intent)
+
+
         }
 
 
