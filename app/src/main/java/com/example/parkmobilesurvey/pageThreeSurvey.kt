d14384de1ed50716c3c58dfaac7557c584a73d23
keyaroz <keyarozar@gmail.com>
2022-05-07 22:19:10 -0400

    Keya 10:19

--- app/src/main/java/com/example/parkmobilesurvey/pageThreeSurvey.kt
+++ app/src/main/java/com/example/parkmobilesurvey/pageThreeSurvey.kt
@@ -1,11 +1,32 @@
 package com.example.parkmobilesurvey
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class pageThreeSurvey : AppCompatActivity() {
+    private lateinit var test : TextView
+    lateinit var preferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page_three_survey)
+
+        test = findViewById(R.id.testText);
+
+
+        preferences = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+        val ans = preferences.getInt("firstAns", 0) // should i put 0
+
+        val selectedItem1 = findViewById<RadioButton>(ans)
+        test.text = selectedItem1.text
+
+
+//        test.setText()
+
     }
 }
