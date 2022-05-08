2cbdfdbf3844f188b1f2eab5bd986486a0a98dd6
keyaroz <keyarozar@gmail.com>
2022-05-08 00:23:12 -0400

    keya 12:21

--- app/src/main/java/com/example/parkmobilesurvey/pageThreeSurvey.kt
+++ app/src/main/java/com/example/parkmobilesurvey/pageThreeSurvey.kt
@@ -9,24 +9,39 @@ import android.widget.*
 
 class pageThreeSurvey : AppCompatActivity() {
     private lateinit var test : TextView
+    private lateinit var editText : EditText
     lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page_three_survey)
 
-        test = findViewById(R.id.testText);
+        val parkName = intent.getStringExtra("park")
+        val firstAns = intent.getStringExtra("A1")
+        val secondAns = intent.getStringExtra("A2")
+        val thirdAns = intent.getStringExtra("A3")
+        val fourthAns = intent.getStringExtra("A4")
+        val fifthAns = intent.getStringExtra("A5")
 
 
-        preferences = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
-        val ans = preferences.getInt("firstAns", 0) // should i put 0
+        editText = findViewById(R.id.editText)
 
-        val selectedItem1 = findViewById<RadioButton>(ans)
-        test.text = selectedItem1.text
+        val sixthAns = editText.text
+        if(sixthAns.length == 0) {
+            Toast.makeText(this, "You must enter feedback", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            Toast.makeText(this, "Survey answers saved", Toast.LENGTH_SHORT).show()
+        }
 
 
-//        test.setText()
+
+
+
+
+
+
+
 
     }
 }
